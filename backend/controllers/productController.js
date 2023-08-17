c24dda510ccaf38ca88e2b071ea49f849f28ee7b
MeghBora <93298770+MeghBora@users.noreply.github.com>
2023-08-18 00:00:23 +0530

    product route Complete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,10 +10,49 @@ exports.createProduct = async (req,res,next) =>{
     });
 }
 
+//editProducts--Admin
+exports.updateProducts = async(req,res)=>{
+    let product = await productModel.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        });
+    }
+
+    product = await productModel.findByIdAndUpdate(req.param.id,req.body,{new:true,runValidators:true, useFindAndModify:false});
+
+    res.status(200).json({
+        success:true,
+        product
+    });
+
+}
+
+//deleteProducts --Admin
+exports.deleteProduct = async(req,res)=>{
+    const product = await productModel.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        });
+    }
+
+    await product.remove();
+    res.status(200).json({
+        success:true,
+        massage:"product deleted successfully"
+    });
+}
+
+//getAllProducts
 exports.getAllProducts= async(req,res) =>{
    const allProduct = await productModel.find({});
    res.status(200).json({
     success:true,
     allProduct
-   })
+   });
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,5 +4,7 @@ const Router = express.Router();
 
 Router.route("/getAllProducts").get(productController.getAllProducts);
 Router.route("/createProduct").post(productController.createProduct);
+Router.route("/updateProducts/:id").put(productController.updateProducts);
+Router.route("/deleteProducts/:id").delete(productController.deleteProduct);
 
 module.exports = Router;
